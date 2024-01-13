f15955d2c224f469eb31ac03729c52bb45082706
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2024-01-13 20:41:43 +0000

    https

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
     "https://perceptron.dev",
-    "http://percms.ukwest.cloudapp.azure.com/",
+    "https://percms.ukwest.cloudapp.azure.com/",
   ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
